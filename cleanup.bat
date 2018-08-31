61e984dbf540943ce391e66a41b74ff2d7958ede
Mauricio Ulargui <mauricio@ulargui.com>
2018-08-31 09:07:40 -0700

    Adding helm support

--- cleanup.bat
+++ cleanup.bat
@@ -1,6 +1,6 @@
 rem you need to run this shellscript as an administrator
 
-rem cleanup
+rem cleanup minikube 
 minikube stop
 minikube delete
 del %systemroot%\system32\kubectl.exe
@@ -8,5 +8,9 @@ del %systemroot%\system32\minikube.exe
 rmdir %userprofile%\.minikube /s /q
 rmdir %userprofile%\.kube /s /q
 rmdir %userprofile%\cluster /s /q
+
+rem cleanup helm
+del %systemroot%\system32\helm.exe
+rmdir %userprofile%\.helm /s /q
 
 exit /B 0
