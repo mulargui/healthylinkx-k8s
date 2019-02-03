45932eea5c7901a280b417f2b6d17dd56bb1960f
mulargui <mauricio@ulargui.com>
2019-02-03 09:15:16 -0800

    refining setup

--- clone.bat
+++ clone.bat
@@ -1,7 +1,8 @@
 
-rem clone the three repos
-git clone https://github.com/mulargui/healthylinkx-mysql.git ..
-git clone https://github.com/mulargui/healthylinkx-api-in-node.git ..
-git clone https://github.com/mulargui/healthylinkx-ux.git ..
+rem clone the three repos one level up, default is in this directory
+cd ..
+git clone https://github.com/mulargui/healthylinkx-mysql.git
+git clone https://github.com/mulargui/healthylinkx-api-in-node.git
+git clone https://github.com/mulargui/healthylinkx-ux.git
 
 exit /B 0
