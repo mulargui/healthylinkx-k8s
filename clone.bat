f831ce306c6057bf3e635023ec5eb826c7ab4b75
mulargui <mauricio@ulargui.com>
2019-02-02 17:34:50 -0800

    refining setup

---
+++ clone.bat
@@ -0,0 +1,7 @@
+
+rem clone the three repos
+git clone https://github.com/mulargui/healthylinkx-mysql.git ..
+git clone https://github.com/mulargui/healthylinkx-api-in-node.git ..
+git clone https://github.com/mulargui/healthylinkx-ux.git ..
+
+exit /B 0
--- setup.bat
+++ setup.bat
@@ -1,9 +1,4 @@
 
-rem clone the three repos
-git clone https://github.com/mulargui/healthylinkx-mysql.git ..
-git clone https://github.com/mulargui/healthylinkx-api-in-node.git ..
-git clone https://github.com/mulargui/healthylinkx-ux.git ..
-
 rem setup each service
 call %~dp0..\healthylinkx-mysql\k8s\setup 
 call %~dp0..\healthylinkx-api-in-node\k8s\setup
